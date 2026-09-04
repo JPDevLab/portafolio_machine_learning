d63dee83cd139219abfa91221a811cd5090ee57f
Juan Pablo Acevedo <163020994+JPDevLab@users.noreply.github.com>
2026-09-03 19:04:36 -0500

    Update image filename for Decision Trees section

--- portafolio_ml.py
+++ portafolio_ml.py
@@ -43,7 +43,7 @@ with col2:
 with col3:
     st.subheader("Árboles de Decisión")
     # 👇 REEMPLAZA el nombre del archivo de imagen
-    image = Image.open("knn.png")
+    image = Image.open("ArbolesDecision.png")
     st.image(image, width=190)
     st.write("En el siguiente enlace veremos cómo funciona un árbol de decisión.")
     # 👇 REEMPLAZA la url por la de tu app en Streamlit Cloud
